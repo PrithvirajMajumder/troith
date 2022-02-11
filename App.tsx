0e8054f5a4bf8cf2593c148fe7a59a46f6064f08
Prithviraj Mazumder <prithviraj@Prithvirajs-MacBook-Air.local>
2022-02-11 18:21:40 +0530

    added react native routing in the app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import tw from "twrnc";
+import Home from './Pages/Home';
+
+const AppStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text style={tw`text-red-500`}>Let's build Troith ❤️</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <AppStack.Navigator>
+          <AppStack.Screen name='Home' component={Home}></AppStack.Screen>
+        </AppStack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
