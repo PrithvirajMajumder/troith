c42b83486988843ca885c8be5f57b9e9ad123494
Prithviraj Mazumder <prithviraj@Prithvirajs-MacBook-Air.local>
2022-02-11 17:15:48 +0530

    added redux native elements and tailwind css

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text style={tw`text-red-500`}>Let's build Troith ❤️</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
